6e48d274868e288eec2b34dfc1b13df18fd8f277
GabrielRf <ti.gabrielrf@gmail.com>
2023-04-13 13:55:30 -0300

    Feat: databaseConfig refactoring

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,13 +1,19 @@
 import "dotenv/config";
 import { MongoMemoryServer } from "mongodb-memory-server";
 
-const mongoTesteServer = new MongoMemoryServer();
+async function getDataBaseConfg() {
+  const mongoServer = await MongoMemoryServer.create();
 
-export const databaseConfig = {
-  dev: {
-    uri: process.env.MONGODB_URL!,
-  },
-  test: {
-    uri: mongoTesteServer.getUri(),
-  },
-};
+  const databaseConfig = {
+    dev: {
+      uri: process.env.MONGODB_URL!,
+    },
+    test: {
+      uri: mongoServer.getUri(),
+    },
+  };
+
+  return databaseConfig;
+}
+
+module.exports = getDataBaseConfg;
