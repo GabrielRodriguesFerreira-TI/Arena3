51072003ac10581c2a542eccab7e59424f730d3e
GabrielRf <ti.gabrielrf@gmail.com>
2023-04-14 14:57:35 -0300

    Feat: remove versionKey in response

--- src/services/users/createUsers.service.ts
+++ src/services/users/createUsers.service.ts
@@ -5,6 +5,7 @@ export const createUserServices = async (
   payload: iCreateUser
 ): Promise<iCreateUserReturn> => {
   const user = await User.create(payload);
+  user.toObject({ versionKey: false });
 
   return user;
 };
