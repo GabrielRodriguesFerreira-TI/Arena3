828743af0f19939022ee4ddfd682683b92fde5cd
GabrielRf <ti.gabrielrf@gmail.com>
2023-04-13 23:11:13 -0300

    Feat: add class AppError with handleErros function

---
+++ src/errors/erros.ts
@@ -0,0 +1,31 @@
+import { NextFunction, Request, Response } from "express";
+
+class AppError extends Error {
+  message: string;
+  statusCode: number;
+
+  constructor(message: string, statusCode: number = 400) {
+    super();
+    this.message = message;
+    this.statusCode = statusCode;
+  }
+}
+
+const handleErros = (
+  error: Error,
+  req: Request,
+  res: Response,
+  _: NextFunction
+) => {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      message: error.message,
+    });
+  }
+
+  return res.status(500).json({
+    message: "Internal server error",
+  });
+};
+
+export { AppError, handleErros };
