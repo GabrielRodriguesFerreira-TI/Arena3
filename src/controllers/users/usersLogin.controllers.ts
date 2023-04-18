f8fc2d2670a8dfa6d7447d3534bbd7607d1e1030
GabrielRf <ti.gabrielrf@gmail.com>
2023-04-18 17:40:32 -0300

    Feat: add userLoginController

---
+++ src/controllers/users/usersLogin.controllers.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from "express";
+import { iLoginUser } from "../../interfaces/usersLogin.types";
+import { createLoginService } from "../../services/usersLogin/createLogin.service";
+
+export const userLoginController = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  const userInfo: iLoginUser = req.body;
+
+  const token: string = await createLoginService(userInfo);
+
+  return res.json(token);
+};
---
+++ src/services/usersLogin/createLogin.service.ts
@@ -0,0 +1,5 @@
+import { iLoginUser } from "../../interfaces/usersLogin.types";
+
+export const createLoginService = async (
+  payload: iLoginUser
+): Promise<any> => {};
