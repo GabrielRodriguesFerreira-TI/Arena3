9b0e50bd724dbb1acf97d252c3bfd358d4d682d7
GabrielRf <ti.gabrielrf@gmail.com>
2023-04-18 20:09:21 -0300

    Feat: add userRefreshTokenService with userRefreshTokenController on route /login/token

--- src/routes/usersLogin.routes.ts
+++ src/routes/usersLogin.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
-import { userLoginController } from "../controllers/users/usersLogin.controllers";
+import {
+  userLoginController,
+  userRefreshTokenController,
+} from "../controllers/users/usersLogin.controllers";
 
 export const usersLoginRoutes: Router = Router();
 
 usersLoginRoutes.post("/login", userLoginController);
 
-usersLoginRoutes.post("/login/token");
+usersLoginRoutes.post("/login/token", userRefreshTokenController);
 
 usersLoginRoutes.post("/logout");
