779c05fc88c39640578021d3406cf09f4254b752
GabrielRf <ti.gabrielrf@gmail.com>
2023-04-18 19:25:02 -0300

    Feat: add userRefreshTokenController with userRefreshTokenService

--- src/controllers/users/usersLogin.controllers.ts
+++ src/controllers/users/usersLogin.controllers.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { iLoginUser } from "../../interfaces/usersLogin.types";
 import { createLoginService } from "../../services/usersLogin/createLogin.service";
+import { userRefreshTokenService } from "../../services/usersLogin/userRefreshToken.service";
 
 export const userLoginController = async (
   req: Request,
@@ -11,4 +12,15 @@ export const userLoginController = async (
   const token: string = await createLoginService(userInfo, res);
 
   return res.json(token);
+};
+
+export const userRefreshTokenController = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  const refreshToken: string = req.cookies.refreshToken;
+
+  const response = await userRefreshTokenService(refreshToken);
+
+  return res.status(200).json(response);
 };
