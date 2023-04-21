de705e3ac2fabd8ef1b508df31c8b0e91d3a2d7d
GabrielRf <ti.gabrielrf@gmail.com>
2023-04-20 22:12:03 -0300

    Feat: add uploadUserProfileImageService

--- src/services/usersLogin/uploadUserProfileImage.service.ts
+++ src/services/usersLogin/uploadUserProfileImage.service.ts
@@ -1,5 +1,14 @@
 import { iUploadUserImageProfile } from "../../interfaces/users.types";
+import { User } from "../../models/User.model";
 
 export const uploadUserProfileImageService = async (
   payload: iUploadUserImageProfile
-): Promise<any> => {};
+): Promise<any> => {
+  const { avatarUrl, userId } = payload;
+
+  const user = await User.findById(userId);
+  user!.imageProfile = avatarUrl;
+  await user!.save();
+
+  return { message: "Image updated successfully!" };
+};
