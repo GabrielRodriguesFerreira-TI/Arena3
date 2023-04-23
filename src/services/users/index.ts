7dc34753d96094e42fd2c7e14a8dd8965121683d
GabrielRf <ti.gabrielrf@gmail.com>
2023-04-23 17:37:54 -0300

    Feat: add index.ts on folder tmp, add index.ts on folders users services, and usersLogin services, add middlewares on usersRoutes

---
+++ src/services/users/index.ts
@@ -0,0 +1,9 @@
+import { createUserServices } from "./createUsers.service";
+import { deleteUserProfileImageService } from "./deleteUserProfileImage.service";
+import { uploadUserProfileImageService } from "./uploadUserProfileImage.service";
+
+export default {
+  createUserServices,
+  deleteUserProfileImageService,
+  uploadUserProfileImageService,
+};
---
+++ src/services/usersLogin/index.ts
@@ -0,0 +1,9 @@
+import { createLoginService } from "./createLogin.service";
+import { userLogoutService } from "./userLogout.service";
+import { userRefreshTokenService } from "./userRefreshToken.service";
+
+export default {
+  createLoginService,
+  userLogoutService,
+  userRefreshTokenService,
+};
