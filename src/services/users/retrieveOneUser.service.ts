0613548b35e5687aefff0bcf025c03c7e12706ee
GabrielRf <ti.gabrielrf@gmail.com>
2023-04-26 15:09:55 -0300

    Feat: add retrieveOneUserService

--- src/services/users/retrieveOneUser.service.ts
+++ src/services/users/retrieveOneUser.service.ts
@@ -1,3 +1,18 @@
 import { Request } from "express";
+import { User } from "../../models/User.model";
+import { iCreateUserReturn } from "../../interfaces/users.types";
 
-export const retrieveOneUserService = async (req: Request): Promise<any> => {};
+export const retrieveOneUserService = async (
+  req: Request
+): Promise<iCreateUserReturn> => {
+  const userId: string = req.params.user_id;
+
+  const user = await User.findOne({
+    _id: userId,
+    deletedAt: { $exists: false },
+  }).select("-isAdmin");
+
+  const userReturn = user!.UserWithoutPassword();
+
+  return userReturn;
+};
