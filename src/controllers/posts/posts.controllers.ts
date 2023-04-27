06e7f6d5864f6a2e70d6457d6ec31b949f2eb8eb
GabrielRf <ti.gabrielrf@gmail.com>
2023-04-27 17:53:32 -0300

    Feat: add uploadPostMidiaService

--- src/controllers/posts/posts.controllers.ts
+++ src/controllers/posts/posts.controllers.ts
@@ -1,14 +1,20 @@
 import { Request, Response } from "express";
 import * as Posts from "../../services/posts/index";
+import { iPayloadUploadMidia } from "../../interfaces/posts/posts.types";
 
 export const uploadPostMidiaController = async (
   req: Request,
   res: Response
 ): Promise<Response> => {
-  const file = req.file!;
-  const userId: string = req.params.user_id;
+  const payload: iPayloadUploadMidia = {
+    file: req.file!,
+    userId: req.params.user_id,
+    fileType: req.params.type,
+    req: req,
+    res: res,
+  };
 
-  const response = await Posts.uploadPostMidiaService(file, userId);
+  const response = await Posts.uploadPostMidiaService(payload);
 
   return res.status(200).json(response);
 };
--- src/services/posts/uploadPostMidia.service.ts
+++ src/services/posts/uploadPostMidia.service.ts
@@ -1,4 +1,31 @@
+import { iPayloadUploadMidia } from "../../interfaces/posts/posts.types";
+import imageProfileMulter from "../../config/imageProfile.multer";
+import postMidiaMulter from "../../config/postMidia.multer";
+import multer from "multer";
+import { AppError } from "../../errors/erros";
+
 export const uploadPostMidiaService = async (
-  file: Express.Multer.File,
-  userId: string
-) => {};
+  payload: iPayloadUploadMidia
+): Promise<{ message: string }> => {
+  const { fileType, req, res } = payload;
+  const uploadImage = multer(imageProfileMulter);
+  const uploadPostMidia = multer(postMidiaMulter.config);
+
+  if (fileType === "video") {
+    uploadPostMidia.single("video")(req, res, (err: unknown) => {
+      if (err) {
+        throw new AppError(`${err}`, 400);
+      }
+    });
+  } else if (fileType === "image") {
+    uploadImage.single("image")(req, res, (err: unknown) => {
+      if (err) {
+        throw new AppError(`${err}`, 400);
+      }
+    });
+  } else {
+    throw new AppError("Invalid file type!", 400);
+  }
+
+  return { message: "Midia successful upload." };
+};
