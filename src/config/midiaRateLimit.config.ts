a5410c711adf78cfe8ab3e8c4b4d0f4e4999f0b7
GabrielRf <ti.gabrielrf@gmail.com>
2023-04-27 22:50:26 -0300

    Feat: add JsonFileStore with LegacyStore

--- src/config/midiaRateLimit.config.ts
+++ src/config/midiaRateLimit.config.ts
@@ -1,8 +1,10 @@
 import { Request, Response } from "express";
 import expressRateLimit from "express-rate-limit";
 import { AppError } from "../errors/erros";
+import JsonFileStore from "./jsonFileStore.config";
 
 export const videoUploadRateLimit = expressRateLimit({
+  store: new JsonFileStore("videoUploadRateLimit.json"),
   windowMs: 30 * 60 * 1000, // 30 minutos
   max: 1,
   handler: function (req: Request, res: Response) {
@@ -14,6 +16,7 @@ export const videoUploadRateLimit = expressRateLimit({
 });
 
 export const imageUploadRateLimit = expressRateLimit({
+  store: new JsonFileStore("imageUploadRateLimit.json"),
   windowMs: 30 * 60 * 1000, // 30 minutos
   max: 3,
   handler: function (req: Request, res: Response) {
