594be1b4938c23d5f3d959e7d2fca8d4b2d3b7a4
GabrielRf <ti.gabrielrf@gmail.com>
2023-05-02 00:34:13 -0300

    Feat: add mock users

---
+++ src/__tests__/mocks/users/deleteUser.route.mock.ts
@@ -0,0 +1,16 @@
+export const userDeleteMock = {
+  userOwner: {
+    username: "A3on",
+    password: "123456",
+    email: "gabrielrf@gmail.com",
+    firstName: "Gabriel",
+    lastName: "Rodrigues",
+  },
+  userIsNotOwner: {
+    username: "Maltohumor",
+    password: "123456",
+    email: "antonio@gmail.com",
+    firstName: "Antonio",
+    lastName: "Santos",
+  },
+};
---
+++ src/__tests__/mocks/users/loginUser.route.mock.ts
@@ -0,0 +1,24 @@
+export const userLoginMock = {
+  userActivate: {
+    username: "A3on",
+    password: "123456",
+    email: "gabrielrf@gmail.com",
+    firstName: "Gabriel",
+    lastName: "Rodrigues",
+  },
+  userToInactive: {
+    username: "Maltohumor",
+    password: "123456",
+    email: "antonio@gmail.com",
+    firstName: "Antonio",
+    lastName: "Santos",
+  },
+  userInvalidCredential1: {
+    email: "gabrielrf@gmail.com",
+    password: "invalid_credentials",
+  },
+  userInvalidCredential2: {
+    email: "invalid_credentials@gmail.com",
+    password: "123456",
+  },
+};
